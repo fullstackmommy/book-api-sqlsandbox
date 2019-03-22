517bb7af355e8de9926290f70fbb349159b8d87c
fullstackmommy <nicole.budiman@gmail.com>
2019-03-22 14:00:31 +0800

    Create Book and Author models

---
+++ models/index.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize("books-api", "postgres", "postgres", {dialect: "postgres"})
+
+const models = {
+    Book: sequelize.import ("./Book"),
+    Author: sequelize.import ("./Author")
+}
+
+Object
+    .keys(models)
+    .forEach(key => {
+        if ("associate" in models[key]) {
+            models[key].associate(models)
+        }
+    })
+
+module.exports = {
+    sequelize,
+    ...models
+}
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const app = require("./app");
+const {sequelize} = require("./models")
+const createBooksAndAuthors = require('./seed')
 
 const port = process.env.PORT || 5555;
+const eraseDatabaseOnSync = true
 
-app.listen(port, () => {
-  if (process.env.NODE_ENV === "production") {
-    console.log(`Server is running on Heroku with port number ${port}`);
-  } else {
-    console.log(`Server is running on http://localhost:${port}`);
-  }
-});
+sequelize
+  .sync({force: eraseDatabaseOnSync})
+  .then(() => {
+    if (eraseDatabaseOnSync) {
+      createBooksAndAuthors()
+    }
+    app.listen(port, () => {
+      if (process.env.NODE_ENV === "production") {
+        console.log(`Server is running on Heroku with port number ${port}`);
+      } else {
+        console.log(`Server is running on http://localhost:${port}`);
+      }
+    });
+  })
